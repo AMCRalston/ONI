3fb68006dc3ce9caa929474af97e22766a5300e6
root <root@localhost.localdomain>
2018-09-10 13:14:10 -0400

    Adding Priv esc doc

--- supergobuster.sh
+++ supergobuster.sh
@@ -5,15 +5,17 @@ URL=$1
 
 echo "super go bustering for super brute: $URL"
 
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/tomcat.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/nginx.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/apache.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/seclists/Discovery/Web_Content/Top1000-RobotsDisallowed.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/ApacheTomcat.fuzz.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/sharepoint.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/iis.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x php 
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x doc 
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x docx
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /usr/share/dirbuster/wordlists/directory-list-2.3-small.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /usr/share/dirb/wordlists/small.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/tomcat.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/nginx.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/apache.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /opt/RobotsDisallowed/Top1000-RobotsDisallowed.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/ApacheTomcat.fuzz.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /usr/share/dirb/wordlists/vulns/sharepoint.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /opt/SecLists/Discovery/Web-Content/iis.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x txt
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x php 
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x doc 
+gobuster -u $URL -l -k -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x docx
